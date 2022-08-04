ca28d62ec21dd0ed03f9d037189154198baabe48
2018180011woochan <45459935+2018180011woochan@users.noreply.github.com>
2022-08-04 11:45:15 +0900

    PlaneAR Test

--- ARTEST4/Assets/ARPlaceOnPlane.cs
+++ ARTEST4/Assets/ARPlaceOnPlane.cs
@@ -9,6 +9,8 @@ public class ARPlaceOnPlane : MonoBehaviour
     public ARRaycastManager arRaycaster;
     public GameObject placeObject;
 
+    GameObject spawnObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +20,32 @@ public class ARPlaceOnPlane : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateCenterObject();
+        //UpdateCenterObject();
+        PlaceObjectByTouch();
+    }
+
+    private void PlaceObjectByTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            if (arRaycaster.Raycast(touch.position, hits, TrackableType.Planes))
+            {
+                Pose hitPose = hits[0].pose;
+
+                if (!spawnObject)
+                {
+                    spawnObject = Instantiate(placeObject, hitPose.position, hitPose.rotation);
+                }
+                else
+                {
+                    spawnObject.transform.position = hitPose.position;
+                    spawnObject.transform.rotation = hitPose.rotation;
+                }
+            }
+        }
     }
 
     private void UpdateCenterObject()
